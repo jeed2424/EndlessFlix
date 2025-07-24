cf072dcf346b91045ad39781a44e4e46c7f84984
jeed2424 <50756835+jeed2424@users.noreply.github.com>
2025-07-24 13:25:38 -0400

    Remove hot-reload for Firefox
    Functions are not supported by browser, only for Chrome

--- Firefox/js/hot-reload.js
+++ Firefox/js/hot-reload.js
@@ -3,41 +3,7 @@
  Reloads the current page anytime the file timestamp changes
 
  Detects when the application is installed in development mode. If it is, monitor all the timestamps in the Extension directory
- Any time the timestamp doesn't match the last one, reload the current tab
+ Any time the timestamp doesn't match the last one, reload the current tab.
+
+ This feature is only available on Chrome. Use built-in tools available at about:debugging on Firefox for similar features.
  */
-const filesInDirectory = dir => new Promise(resolve =>
-  dir.createReader().readEntries(entries =>
-    Promise.all(entries.filter(e => e.name[0] !== '.').map(e =>
-      e.isDirectory ? filesInDirectory(e) : new Promise(resolve => e.file(resolve))))
-      .then(files => [].concat(...files))
-      .then(resolve)));
-
-const timestampForFilesInDirectory = dir => filesInDirectory(dir).then(files => files.map(f => f.name + f.lastModifiedDate).join());
-
-const reload = () => {
-  browser.tabs.query({
-    active: true,
-    currentWindow: true
-  }, tabs => {
-    if (tabs[0]) {
-      browser.tabs.reload(tabs[0].id);
-    }
-    browser.runtime.reload();
-  });
-};
-
-const watchChanges = (dir, lastTimestamp) => {
-  timestampForFilesInDirectory(dir).then(timestamp => {
-    if (!lastTimestamp || (lastTimestamp === timestamp)) {
-      setTimeout(() => watchChanges(dir, timestamp), 1000); // retry after 1s
-    } else {
-      reload();
-    }
-  });
-};
-
-browser.management.getSelf(self => {
-  if (self.installType === 'development') {
-    browser.runtime.getPackageDirectoryEntry(dir => watchChanges(dir));
-  }
-});
